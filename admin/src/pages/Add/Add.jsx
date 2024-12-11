9693948059ea6643aa5e9144ac412f700b4ea233
Saloni Kumari <122854015+saloni-s6@users.noreply.github.com>
2024-12-11 11:32:25 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -105,7 +105,7 @@ const Add = ({url}) => {
               value={data.price}
               type="Number"
               name="price"
-              placeholder="$20"
+              placeholder="₹100"
             />
           </div>
         </div>

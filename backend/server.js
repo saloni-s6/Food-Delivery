b814975dcdd75f032ef0b254b16ad4861ecfc601
Saloni Kumari <122854015+saloni-s6@users.noreply.github.com>
2025-04-18 10:45:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
+import geminiRouter from "./routes/geminiRoute.js"
 
 
 
@@ -26,6 +27,7 @@ app.use("/images",express.static('uploads'))
 app.use("/api/user", userRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
+app.use("/api/gemini", geminiRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
